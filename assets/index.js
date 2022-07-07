9891748e719eb20053832e467a69201852d4dd50
Gabriel Renard <gabriel.renard@protonmail.com>
2022-07-07 13:22:58 +0200

    added basic logic to index.js

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,47 @@
-const selectionButtons = document.querySelectorAll("[data-select]");
+const choices = document.querySelectorAll("button");
+const userSelectionDisplay = document.querySelector("#user-selection");
+const computerSelectionDisplay = document.querySelector("#computer-selection");
+const result = document.querySelector("#result");
 
-selectionButtons.forEach(selectButton => {
-  selectButton.addEventListener("click", e => {
-    const selectionName = selectButton.dataset.select;
-    makeSelection(selectionName);
-  });
-});
+const choicesArr = ["rock", "paper", "scissors"];
 
-function makeSelection(selection) {
-  console.log(selection);
+let userChoice;
+let computerChoice;
+let finalResult;
+
+choices.forEach(choice =>
+  choice.addEventListener("click", e => {
+    userChoice = e.target.id;
+    userSelectionDisplay.innerHTML = userChoice;
+    computerSelection();
+    endResult();
+  })
+);
+
+function computerSelection() {
+  const randomIndex = Math.floor(Math.random() * choicesArr.length);
+  computerChoice = choicesArr[randomIndex];
+  if (randomIndex === 0) {
+    computerChoice = "rock";
+  } else if (randomIndex === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissor";
+  }
+  computerSelectionDisplay.innerHTML = computerChoice;
+}
+
+function endResult() {
+  if (computerChoice === userChoice) {
+    finalResult = "Its a tie!";
+  } else if (computerChoice === "rock" && userChoice === "paper") {
+    finalResult = "You win!";
+  } else if (computerChoice === "scissor" && userChoice === "rock") {
+    finalResult = "You win!";
+  } else if (computerChoice === "paper" && userChoice === "scissor") {
+    finalResult = "You win!";
+  } else {
+    finalResult = "You lost!";
+  }
+  result.innerHTML = finalResult;
 }
